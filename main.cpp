8064fde4d2cf781277e456eafc1e7394fc81e4c0
Chris <crichmond2@mail.csuchico.edu>
2018-09-19 11:18:34 -0700

    Refactored main to include player class. Resolves #1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <vector>
-
+#include "player.h"
 int main(int argc, char *argv[])
 {
-  std::vector<std::string> names;
-  std::vector<int> scores;
-
+  //std::vector<std::string> names;
+  //std::vector<int> scores;
+  Player player;
   if( argc > 1 )
   {
     //Collect player names from command-line arguments
     for(int i=1; i<argc; i++)
     {
-      names.push_back(argv[i]);
+      player.names.push_back(argv[i]);
     }
 
     //Get player scores from user input
-    scores.resize(names.size());
-    for(int i=0; i<names.size(); i++)
+    player.scores.resize(player.names.size());
+    for(int i=0; i<player.names.size(); i++)
     {
-      std::cout<<"Score for "<<names[i]<<": ";
-      std::cin>>scores[i];
+      std::cout<<"Score for "<<player.names[i]<<": ";
+      std::cin>>player.scores[i];
     }
 
     //Print summary
     std::cout<<"### SCOREBOARD ###\n";
-    for(int i=0; i<names.size(); i++)
+    for(int i=0; i<player.names.size(); i++)
     {
-      std::cout<<names[i]<<"  ";
-      std::cout<<scores[i]<<std::endl;
+      std::cout<<player.names[i]<<"  ";
+      std::cout<<player.scores[i]<<std::endl;
     }
   }
   else
---
+++ player.h
@@ -0,0 +1,14 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::string;
+class Player{
+  public:
+    vector<string> names;
+    vector<int> scores;
+
+
+};
+#endif
