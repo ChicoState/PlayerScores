ede995278c17baf68c268f79ed18162ccdfef3c9
Amritpal S Sidhu <asidhu2@ecc-jaguar.csuchico.edu>
2018-09-19 11:33:41 -0700

    Resolved Issue #1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
 #include <vector>
 
+using std::string;
+using std::vector;
+
+class Player {
+    public:
+            string name;
+            int scores;
+            Player(string n, int s);
+};
+
+Player::Player(string n, int s){
+    name = n;
+    scores = s;
+}
+
+
 int main(int argc, char *argv[])
 {
-  std::vector<std::string> names;
-  std::vector<int> scores;
+  vector<Player> players;
+
 
   if( argc > 1 )
   {
     //Collect player names from command-line arguments
     for(int i=1; i<argc; i++)
     {
-      names.push_back(argv[i]);
+      players.push_back(Player(argv[i],0));
     }
 
     //Get player scores from user input
-    scores.resize(names.size());
-    for(int i=0; i<names.size(); i++)
+    for(int i=0; i<players.size(); i++)
     {
-      std::cout<<"Score for "<<names[i]<<": ";
-      std::cin>>scores[i];
+      std::cout<<"Score for "<<players[i].name<<": ";
+      std::cin>>players[i].scores;
     }
 
     //Print summary
     std::cout<<"### SCOREBOARD ###\n";
-    for(int i=0; i<names.size(); i++)
+    for(int i=0; i<players.size(); i++)
     {
-      std::cout<<names[i]<<"  ";
-      std::cout<<scores[i]<<std::endl;
+      std::cout<<players[i].name<<"  ";
+      std::cout<<players[i].scores<<std::endl;
     }
   }
   else
