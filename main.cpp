2c7884f3373e10bd5b0078808e091e68cd80194f
jwright800 <jwright55@mail.csuchico.edu>
2018-09-19 11:45:58 -0700

    Add Player class

--- main.cpp
+++ main.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
 #include <vector>
 
+
+struct Player {
+  std::string names;
+  int scores;
+};
+
 int main(int argc, char *argv[])
 {
-  std::vector<std::string> names;
-  std::vector<int> scores;
+  // std::vector<std::string> names;
+  // std::vector<int> scores;
+  std::vector<Player> players;
 
   if( argc > 1 )
   {
     //Collect player names from command-line arguments
     for(int i=1; i<argc; i++)
     {
-      names.push_back(argv[i]);
+      std::string temp = argv[i];
+      players.push_back(Player({temp, 0}));
     }
 
     //Get player scores from user input
-    scores.resize(names.size());
-    for(int i=0; i<names.size(); i++)
+    for(int i=0; i<players.size(); i++)
     {
-      std::cout<<"Score for "<<names[i]<<": ";
-      std::cin>>scores[i];
+      std::cout<<"Score for "<<players[i].names<<": ";
+      std::cin>>players[i].scores;
     }
 
     //Print summary
     std::cout<<"### SCOREBOARD ###\n";
-    for(int i=0; i<names.size(); i++)
+    for(int i=0; i<players.size(); i++)
     {
-      std::cout<<names[i]<<"  ";
-      std::cout<<scores[i]<<std::endl;
+      std::cout<<players[i].names<<"  ";
+      std::cout<<players[i].scores<<std::endl;
     }
   }
   else
