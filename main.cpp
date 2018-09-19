5b08d570164d616c0439c127075be3413587844f
Pactionly <21381891+Pactionly@users.noreply.github.com>
2018-09-19 11:22:13 -0700

    Merges names and scores into a single Player class. Closes #1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,66 @@
 #include <iostream>
 #include <vector>
 
+class Player
+{
+ private:
+  std::string name;
+  int score;
+
+ public:
+  Player()
+  {
+    name ="";
+    score = 0;
+  }
+
+  void setName(std::string name)
+  {
+    this->name = name;
+  }
+  void setScore(int score)
+  {
+    this->score = score;
+  }
+  std::string getName()
+  {
+    return name;
+  }
+  int getScore()
+  {
+    return score;
+  }
+};
+
 int main(int argc, char *argv[])
 {
-  std::vector<std::string> names;
-  std::vector<int> scores;
+  std::vector<Player> players;
+  Player temp;
 
   if( argc > 1 )
   {
     //Collect player names from command-line arguments
     for(int i=1; i<argc; i++)
     {
-      names.push_back(argv[i]);
+      temp.setName(argv[i]);
+      players.push_back(temp);
     }
 
     //Get player scores from user input
-    scores.resize(names.size());
-    for(int i=0; i<names.size(); i++)
+    int tempScore;
+    for(unsigned int i=0; i < players.size(); i++)
     {
-      std::cout<<"Score for "<<names[i]<<": ";
-      std::cin>>scores[i];
+      std::cout << "Score for " << players[i].getName() << ": ";
+      std::cin >> tempScore;
+      players[i].setScore(tempScore);
     }
 
     //Print summary
-    std::cout<<"### SCOREBOARD ###\n";
-    for(int i=0; i<names.size(); i++)
+    std::cout << "### SCOREBOARD ###\n";
+    for(unsigned int i=0; i < players.size(); i++)
     {
-      std::cout<<names[i]<<"  ";
-      std::cout<<scores[i]<<std::endl;
+      std::cout << players[i].getName() << "  ";
+      std::cout << players[i].getScore() << std::endl;
     }
   }
   else
