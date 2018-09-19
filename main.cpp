593f9667b2a955824a6b3b2cca22dd1b1474bc26
Doug Andres <dandres1@mail.csuchico.edu>
2018-09-19 11:44:24 -0700

    Refactored code to use a player class, fixes #1
    Added the player class to fix #1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,62 @@
 #include <iostream>
 #include <vector>
 
+class Player {
+  public:
+    std::string name;
+    std::string score;
+
+    void setName(std::string nameInput) {
+      name = nameInput;
+    }
+
+    void setScore(std::string scoreInput) {
+      score = scoreInput;
+    }
+
+    std::string getName() {
+      return name;
+    }
+
+    std::string getScore() {
+      return score;
+    }
+
+};
+
 int main(int argc, char *argv[])
 {
-  std::vector<std::string> names;
-  std::vector<int> scores;
+  Player player1;
+
+  std::vector<Player> players;
+  //std::vector<int> scores;
 
   if( argc > 1 )
   {
     //Collect player names from command-line arguments
     for(int i=1; i<argc; i++)
     {
-      names.push_back(argv[i]);
+      //names.push_back(argv[i]);
+      player1.setName(argv[i]);
+      players.push_back(player1);
     }
 
     //Get player scores from user input
-    scores.resize(names.size());
-    for(int i=0; i<names.size(); i++)
+    //scores.resize(names.size());
+    for(int i=0; i<players.size(); i++)
     {
-      std::cout<<"Score for "<<names[i]<<": ";
-      std::cin>>scores[i];
+      std::string temp;
+      std::cout<<"Score for "<< /*names[i]*/ players[i].getName() <<": ";
+      std::cin>> temp;
+      players[i].setScore(temp);
     }
 
     //Print summary
     std::cout<<"### SCOREBOARD ###\n";
-    for(int i=0; i<names.size(); i++)
+    for(int i=0; i<players.size(); i++)
     {
-      std::cout<<names[i]<<"  ";
-      std::cout<<scores[i]<<std::endl;
+      std::cout<</*names[i]*/ players[i].getName()<<"  ";
+      std::cout<</*scores[i]*/ players[i].getScore()<<std::endl;
     }
   }
   else
