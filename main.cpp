5768c8ebcc385576b7f38aabead5ceb61225bbba
AlexBarnum <alexbarnum24@gmail.com>
2018-09-19 11:42:35 -0700

    Encapsulated player data into a single class.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 #include <vector>
 
+class Player{
+	std::string playerName;
+	int playerScore;
+	public:
+	void setName(std::string name);
+	void setScore(int score);
+	std::string name(){return playerName;};
+	int score(){return playerScore;};
+};
+
+void Player::setName(std::string name){
+	playerName = name;
+}
+
+void Player::setScore(int score){
+	playerScore = score;	
+}
+
 int main(int argc, char *argv[])
 {
-  std::vector<std::string> names;
-  std::vector<int> scores;
-
+  Player players[argc];
   if( argc > 1 )
   {
     //Collect player names from command-line arguments
     for(int i=1; i<argc; i++)
     {
-      names.push_back(argv[i]);
+      Player guy;
+      guy.setName(argv[i]);
+      players[i-1] = guy;
     }
 
     //Get player scores from user input
-    scores.resize(names.size());
-    for(int i=0; i<names.size(); i++)
+    for(int i=0; i<argc - 1; i++)
     {
-      std::cout<<"Score for "<<names[i]<<": ";
-      std::cin>>scores[i];
+      int n = 0;
+      std::cout<<"Score for "<<players[i].name()<<": ";
+      std::cin>>n;
+      players[i].setScore(n);
     }
 
     //Print summary
     std::cout<<"### SCOREBOARD ###\n";
-    for(int i=0; i<names.size(); i++)
+    for(int i=0;i < argc - 1; i++)
     {
-      std::cout<<names[i]<<"  ";
-      std::cout<<scores[i]<<std::endl;
+      std::cout<<players[i].name()<<"  ";
+      std::cout<<players[i].score()<<std::endl;
     }
   }
   else
@@ -39,3 +58,4 @@ int main(int argc, char *argv[])
 
   return 0;
 }
+
