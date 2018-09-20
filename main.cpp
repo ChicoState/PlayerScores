c3d4582eb7b28cd666e006aed778deae471b171a
Cxwx23 <cwarner13@mail.csuchico.edu>
2018-09-20 15:45:38 -0700

    add files via upload

--- main.cpp
+++ main.cpp
@@ -1,33 +1,54 @@
 #include <iostream>
 #include <vector>
 
+struct Player
+{
+  std::string name;
+  int score;
+
+};
+
 int main(int argc, char *argv[])
 {
-  std::vector<std::string> names;
-  std::vector<int> scores;
+  //std::vector<std::string> names;
+  //std::vector<int> scores;
+  std::vector<Player> players;
 
   if( argc > 1 )
   {
     //Collect player names from command-line arguments
     for(int i=1; i<argc; i++)
     {
-      names.push_back(argv[i]);
+      Player* temp = new Player;
+      temp->name = argv[i];
+      temp->score = 0;
+
+      players.push_back(*temp);
+      delete temp;
     }
 
     //Get player scores from user input
-    scores.resize(names.size());
-    for(int i=0; i<names.size(); i++)
+    //scores.resize(names.size());
+
+    for(int i=0; i < players.size(); i++)
     {
-      std::cout<<"Score for "<<names[i]<<": ";
-      std::cin>>scores[i];
+      //std::cout<<"Score for "<<names[i]<<": ";
+      //std::cin>>scores[i];
+
+      std::cout<<"Score for " << players[i].name << ": ";
+      std::cin >> players[i].score;
+
     }
 
     //Print summary
     std::cout<<"### SCOREBOARD ###\n";
-    for(int i=0; i<names.size(); i++)
+    //for(int i=0; i<names.size(); i++)
+    for(int i = 0; i < players.size(); i++)
     {
-      std::cout<<names[i]<<"  ";
-      std::cout<<scores[i]<<std::endl;
+      //std::cout<<names[i]<<"  ";
+      //std::cout<<scores[i]<<std::endl;
+      std::cout<<players[i].name << "  ";
+      std::cout<<players[i].score<<std::endl;
     }
   }
   else
